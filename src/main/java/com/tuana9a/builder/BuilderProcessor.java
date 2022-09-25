736ba4c43081203d53e7543646a73040cf05a119
Tuan Nguyen Minh <tuana9a@gmail.com>
2022-09-25 07:32:41 +0700

    v1.0.1 - using reflection instead of setter

--- src/main/java/com/tuana9a/builder/BuilderProcessor.java
+++ src/main/java/com/tuana9a/builder/BuilderProcessor.java
@@ -11,84 +11,116 @@ import javax.tools.JavaFileObject;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 // NOTE: can't overwrite class like lombok
 @SupportedAnnotationTypes("com.tuana9a.builder.annotations.Builder")
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 public class BuilderProcessor extends AbstractProcessor {
-    private static final Logger logger = Logger.getLogger(BuilderProcessor.class.getName());
+    private static class PropNameAndPropType {
+        private String propName;
+        private String propType;
+
+        public PropNameAndPropType(String propName, String propType) {
+            this.propName = propName;
+            this.propType = propType;
+        }
+
+        public String getPropName() {
+            return propName;
+        }
+
+        public String getPropType() {
+            return propType;
+        }
+
+        @Override
+        public String toString() {
+            return "{" +
+                    "propName='" + propName + '\'' +
+                    ", propType='" + propType + '\'' +
+                    '}';
+        }
+    }
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        logger.addHandler(new ConsoleHandler());
         for (TypeElement annotation : annotations) {
             Set<? extends Element> annotatedElements = roundEnv.getElementsAnnotatedWith(annotation)
                     .stream()
                     .filter(element -> element.getKind().isClass())
                     .collect(Collectors.toSet());
             for (Element element : annotatedElements) {
-                String fullClassName = ((TypeElement) element).getQualifiedName().toString();
+                String className = ((TypeElement) element).getQualifiedName().toString();
                 try {
-                    List<Element> srcPropElements = element.getEnclosedElements()
+                    List<PropNameAndPropType> propNameAndPropTypes = element.getEnclosedElements()
                             .stream()
                             .filter(prop -> prop.getKind().isField())
+                            .map(prop -> new PropNameAndPropType(prop.getSimpleName().toString(), prop.asType().toString()))
                             .collect(Collectors.toList());
-                    Map<String, String> srcPropMap = srcPropElements.stream()
-                            .collect(Collectors.toMap(
-                                    prop -> prop.getSimpleName().toString(),
-                                    prop -> prop.asType().toString()
-                            ));
-                    String result = writeBuilderClass(fullClassName, srcPropMap);
-                    logger.info("[DONE] " + BuilderProcessor.class.getName() + " process: " + fullClassName + " output: " + result);
+                    String result = writeBuilderClass(className, propNameAndPropTypes);
+                    System.out.println("[DONE] " + BuilderProcessor.class.getName() + ": " + className + " -> " + result);
                 } catch (IOException e) {
-                    logger.warning("[FAILED] " + BuilderProcessor.class.getName() + " process " + fullClassName + " error: " + e.getMessage());
+                    System.err.print("[FAILED] " + BuilderProcessor.class.getName() + ": " + className + ": " + e.getMessage());
+                    e.printStackTrace();
                 }
             }
         }
         return true;
     }
 
-    private String writeBuilderClass(String fullClassName, Map<String, String> propsMap) throws IOException {
-        String packageName = null;
-        int lastDot = fullClassName.lastIndexOf('.');
-        if (lastDot > 0) {
-            packageName = fullClassName.substring(0, lastDot);
-        }
+    private String writeBuilderClass(String className, List<PropNameAndPropType> propNameAndPropTypes) throws IOException {
+        String packageName = className.substring(0, className.lastIndexOf('.'));
+        String simpleClassName = className.substring(className.lastIndexOf('.') + 1);
+        String builderClassName = className + "Builder";
+        String simpleBuilderClassName = builderClassName.substring(className.lastIndexOf('.') + 1);
 
-        String className = fullClassName.substring(lastDot + 1);
-        String fullBuilderClassName = fullClassName + "Builder";
-        String builderClassName = fullBuilderClassName.substring(lastDot + 1);
-
-        JavaFileObject builderFile = processingEnv.getFiler().createSourceFile(fullBuilderClassName);
+        JavaFileObject builderFile = processingEnv.getFiler().createSourceFile(builderClassName);
 
         try (PrintWriter writer = new PrintWriter(builderFile.openWriter())) {
-
-            if (packageName != null) {
-                writer.println("package " + packageName + ";\n");
-            }
-
-            writer.println("public class " + builderClassName + " {\n");
-            writer.println("    private " + className + " object = new " + className + "();\n");
-
-            writer.println("    public " + className + " build() { return object; }\n");
-
-            propsMap.forEach((propName, propType) -> {
-                String setMethod = "set" + propName.substring(0, 1).toUpperCase() + propName.substring(1);
-                writer.println("    public " + builderClassName + " " + propName + "(" + propType + " value) {");
-                writer.println("        object." + setMethod + "(value);");
+            writer.println("package " + packageName + ";");
+            writer.println();
+            writer.println("import java.lang.reflect.Field;");
+            writer.println("import java.lang.reflect.Constructor;");
+            writer.println("import java.lang.reflect.Parameter;");
+            writer.println("import java.util.Map;");
+            writer.println("import java.util.HashMap;");
+            writer.println("import " + className + ";");
+            writer.println();
+            writer.println("public class " + simpleBuilderClassName + " {");
+            writer.println("    private final Map<String, Object> parts;");
+            writer.println();
+            writer.println("    public " + simpleClassName + " build() throws IllegalAccessException {");
+            writer.println("        Class<" + simpleClassName + "> klass = " + simpleClassName + ".class;");
+            writer.println("        " + simpleClassName + " object = new " + simpleClassName + "();"); // TODO: construct by reflection
+            writer.println("        Field[] fields = klass.getDeclaredFields();");
+            writer.println("        for (Field field: fields) {");
+            writer.println("            String fieldName = field.getName();");
+            writer.println("            Object value = this.parts.get(fieldName);");
+            writer.println("            field.setAccessible(true);");
+            writer.println("            field.set(object, value);");
+            writer.println(("       }"));
+            writer.println(("       return object;"));
+            writer.println(("   }"));
+            writer.println();
+            writer.println("    public " + simpleBuilderClassName + "() {");
+            writer.println("        this.parts = new HashMap<>();");
+            writer.println(("   }"));
+            writer.println();
+            for (PropNameAndPropType x : propNameAndPropTypes) {
+                String propName = x.getPropName();
+                String propType = x.getPropType();
+                writer.println("    public " + simpleBuilderClassName + " " + propName + "(" + propType + " value) {");
+                writer.println("        this.parts.put(\"" + propName + "\", value);");
                 writer.println("        return this;");
-                writer.println("    }\n");
-            });
-
+                writer.println("    }");
+                writer.println();
+            }
             writer.println("}");
         }
 
-        return fullBuilderClassName;
+        return builderClassName;
     }
 
 }
